70b2d1fcad77cc872e01244adb9e44776834f2dc
drgvszi <33577111+drgvszi@users.noreply.github.com>
2021-03-08 19:10:43 +0200

    The Beginning

---
+++ Tales Of Neko/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D RB;
+    Vector2 mvm;
+    public Animator animator;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        mvm = Vector2.zero;
+        mvm.x=  Input.GetAxisRaw("Horizontal");
+        mvm.y = Input.GetAxisRaw("Vertical");
+        if (mvm != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", mvm.x);
+            animator.SetFloat("Vertical", mvm.y);
+            animator.SetBool("moving", true);
+            animator.SetFloat("Speed", mvm.sqrMagnitude);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+    void FixedUpdate()
+    {
+        RB.MovePosition(RB.position+mvm*moveSpeed*Time.fixedDeltaTime);
+    }
+}
