98021f0c23894fc7c7f94b5e6a78458bfd71c3fb
drgvszi <33577111+drgvszi@users.noreply.github.com>
2021-04-21 13:47:46 +0300

    SpecUpd

---
+++ Tales Of Neko/Assets/Scripts/DONTDESTROYSOUND.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DONTDESTROYSOUND : MonoBehaviour
+{
+    private static DONTDESTROYSOUND instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
